0edfd720ee5ee58deba9a1ed2792645497643634
Mahmoud <50506318+Mahmoud514@users.noreply.github.com>
2019-08-03 19:08:44 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+## Now you can download the most powerful tools using this simple script
+### Screenshot
+![Screen](http://www.m9c.net/uploads/15648511011.png)
+### Installations
+```
+git clone https://github.com/Mahmoud514/TeamSchoolTermux.git
+cd TeamSchoolTermux
+bash TeamSchoolTermux
+wait in install
+```
+### My Account
+* [TELEGRAM](@Ananym0us)
+* [MyPhone](201062884365)
+* [MyGitHub](https://github.com/Mahmoud514)
---
+++ TeamSchoolTermux.sh.sh
@@ -0,0 +1,386 @@
+##TeamSchoolTermux.sh - Team v1.1
+#!/bin/bash
+#coloer
+r="\033[1;31m"
+g="\033[1;32m"
+b="\033[1;34m"
+p="\033[1;35m"
+c="\033[1;36m"
+w="\033[1;37m"
+y="\033[1;33m"
+n=install
+pkg $n figlet
+pkg $n toilet
+clear
+echo -e "$green"
+figlet -f big "Mahmoud"
+sleep 1
+clear
+apt-get update -y
+clear
+apt upgrade -y
+clear
+pkg install figlet -y
+echo -e "$green"
+clear
+figlet -f small installation
+sleep 1
+figlet -f small 'of'
+sleep 1
+figlet -f small TeamTermux
+sleep 1
+figlet -f small Framwork
+sleep 1
+echo "Installing requirements........"
+sleep 1
+apt-get install curl -y
+apt-get install perl -y
+apt-get install git -y
+clear
+apt-get install python -y
+apt-get install python2 -y
+apt-get install php -y
+clear
+apt-get install unzip -y
+clear
+figlet -f small "Please Wait"
+sleep 3
+termux-setup-storage
+sleep 6
+clear
+toilet -f mono12 -F gay "7"
+sleep 0.5
+toilet -f mono12 -F gay ".O"
+sleep 0.5
+toilet -f mono12 -F gay "..D"
+sleep 0.5
+toilet -f mono12 -F gay "...A"
+sleep 0.5
+toilet -f mono12 -F gay "....T"
+sleep 0.5
+toilet -f mono12 -F gay ".....O"
+sleep 0.5
+toilet -f mono12 -F gay "......P"
+sleep 0.5
+clear
+toilet -f mono12 -F gay "7ODATOP"
+sleep 1
+clear
+toilet -f mono12 -F gay "DONE."
+sleep 1
+clear
+toilet -f mono12 -F gay "Top"
+sleep 0.2
+toilet -f mono12 -F gay "open"
+sleep 0.2
+toilet -f mono12 -F gay "TeamSchool"
+sleep 0.2
+clear
+figlet -f small "Done"
+sleep 1
+
+echo -e "
+
+         $y  BY$r :$g Ma7Moud                $y Telegram$r :$g @Ananym0us
+                             $g      .___.
+     $y          /)          $g     ,-^     ^-.
+     $y         //  $g T$y E$r A$y M$g M$g  /           \     $g M$y A$r H$p M$c O$w U$b D
+     $g--------| |---------$r-----/  __     __  \----$g---------------.__
+     $g|WMWMWMW| |$y>>>>>>>>>$r>>>> | />>\   />>\ |>>>>$g>>>>>>>>>>>>>>>>>>:>
+     $g--------| |---------$r-----| \__/   \__/ |----$g------------------
+     $y         \\            $g    \  $y  /|\ $g   /     By:-Mahmoud
+     $y          \)           $g    \ $y  \_/ $g  /     Version 1.1
+                             $g    |       |    Enjoy....
+                             $g    |$r+H+H+H+$g|
+                             $g    \       /
+                             $g     ^-----^
+
+$r1$w-----------------------------: INSTAll TOOLS :-------------------------------$y
+$w 0 -$y install$g Card-Number                                                           
+$w 1 -$y install$g Nmap
+$w 2 -$y install$g IPGeoLocation      
+$w 3 -$y install$g Sqlmap
+$w 4 -$y install$g Alpine
+$w 5 -$y install$g Upuntu
+$w 6 -$y install$g Fedora
+$w 7 -$y install$g Parrot
+$w 8 -$y install$g Kali
+$w 9 -$y install$g Kali NetHunter
+$w 10-$y install$g Metasploit
+$w 11-$g Buckup-Termux
+$w 12-$g UN Buckup-Termux $r
+$w 13-$y install$g horor                    $w 22-$y install$g CNK-DDOS
+$w 14-$y install$g dicky                    $w 23-$y install$g waspam
+$w 15-$y install$g MasterMarket                    $w 24-$y install$g instagram-tools
+$w 16-$y install$g evil-twin                    $w 25-$y install$g pish
+$w 17-$y install$g new                   $w 26-$y install$g shellphish
+$w 18-$y install$g Exploit-Android                    $w 27-$y install$g Dork-Jahat
+$w 19-$y install$g MyServer                    $w 28-$y install$g Hack-Tools
+$w 20-$y install$g FB-Cracker                    $w 29-$y install$g backdoor-apk
+$w 21-$y install$g Call-Spam                    $w 30-$y install$g Report
+$w 31-$y install$g tshark                    $w 32-$y install$g spykey
+$w 33-$y install$g RED_HAWK                    $w 34-$y install$g CyberScan
+$w 35-$y install$g zarp                    $w 36-$y install$g interfarma
+$w 37-$y install$g refploitv2                    $w 38-$y install$g sosmed
+$w 39-$y install$g ubuntu                    $w 40-$y install$g thechoice
+$w 41-$y install$g EasY_HaCk                    $w 42-$y install$g IPGeoLocation
+$w 43-$y install$g wifi-hacker                    $w 44-$y install$g maps
+$w 45-$y install$g Spammer-Grab                    $w 46-$y install$g Lazymux
+$w 47-$y install$g dirsearch                    $w 48-$y install$g MRKING
+$w 49-$y install$g dagon                    $w 50-$y install$g guardn
+
+$r1$w-----------------------------: INSTAll TOOLS POWER:-------------------------------$y
+$w 51-$g virus_bot                          $w 52-$g TmX-FW
+$w 53-$g fsociety                          $w 54-$g whatshack
+$w 55-$g AK47                          $w 56-$g LittleBrother
+$w 57-$g bugreport                          $w 58-$g Breacher
+$w 59-$g DarkSploit                          $w 60-$g TOOL-DZ
+
+$r1$w-----------------------------: INSTAll TOOLS Hacking Ph:-------------------------------$y
+$w 61-$g BlackHacker               $w 62-$g spy-share
+$w 63-$g King-Hacking              $w 64-$g SPY-KING
+
+$r1$w-----------------------------: INSTAll TOOLS Vairous.apk:-------------------------------$y
+$w 65-$g Amer                     $w 67-$g Malicious
+$w 66-$g vbug                      $w 68-$g AHT.Hacker
+
+$r1$w-----------------------------: INSTAll TOOLS format ph:-------------------------------$y
+$w 69-$g new                      $w 70-$g jmaxojan
+
+                    $w 71-$g Exit in TeamSchoolTermux
+
+ ""####################### ""
+ "By:-Mahmoud"                "Version 1.1"
+                   "Enjoy...."
+                   "Updated"
+ "Thx Download This Script By Mahmoud . "
+ " My Number:-  +201062884365"
+ "My Github:- https://github.com/Mahmoud514"
+ "My Telegram:- @Ananym0us"
+"##########################"
+"
+echo "\033[32;1m"
+read -p "root@T0oLS-Mahmoud --># " u
+echo
+if [ $u -eq 0 ]; then
+git clone https://github.com/INDOnimous/Card-Number.git
+fi
+if [ $u -eq 1 ]; then
+pkg install nmap -y
+fi
+if [ $u -eq 2 ]; then
+git clone https://github.com/maldevel/IPGeoLocation -y
+fi
+if [ $u -eq 3 ]; then
+git clone https://github.com/sqlmapproject/sqlmap -y
+fi
+if [ $u -eq 4 ]; then
+pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Alpine/alpine.sh && bash alpine.sh
+fi
+if [ $u -eq 5 ]; then
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
+fi
+if [ $u -eq 6 ]; then
+pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Fedora/fedora.sh && bash fedora.sh
+fi
+if [ $u -eq 7 ]; then
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Parrot/parrot.sh && bash parrot.sh
+fi
+if [ $u -eq 8 ]; then
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+fi
+if [ $u -eq 9 ]; then
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Nethunter/nethunter.sh && bash nethunter.sh
+fi
+if [ $u -eq 10 ]; then
+pkg install unstable-repo -y && pkg install metasploit -y
+fi
+if [ $u -eq 11 ]; then
+termux-setup-storage && cd /data/data/com.termux/files && tar -cvzf /sdcard/buckup-termux.tgz home usr
+fi
+if [ $u -eq 12 ]; then
+termux-setup-storage && cd /data/data/com.termux/files &&  tar -xvzf /sdcard/buckup-termux.tgz
+fi
+if [ $u -eq 13 ]; then
+git clone https://github.com/sachin175638/horor
+fi
+if [ $u -eq 14 ]; then
+git clone https://github.com/DICKYalone/dicky.git
+fi
+if [ $u -eq 15 ]; then
+git clone https://github.com/seifsherifssss/MasterMarket
+fi
+if [ $u -eq 16 ]; then
+git clone https://github.com/soyalk/clone-download-alk-evil-twin.git
+fi
+if [ $u -eq 17 ]; then
+git clone https://github.com/yehia-hacker/new
+fi
+if [ $u -eq 18 ]; then
+git clone https://github.com/fireworm0/Exploit-Android-Stagefright
+fi
+if [ $u -eq 19 ]; then
+git clone https://github.com/Rajkumrdusad/MyServer.git
+fi
+if [ $u -eq 20 ]; then
+git clone https://github.com/hatakecnk/FB-Cracker
+fi
+if [ $u -eq 21 ]; then
+git clone https://github.com/hatakecnk/Call-Spam
+fi
+if [ $u -eq 22 ]; then
+git clone https://github.com/hatakecnk/CNK-DDOS
+fi
+if [ $u -eq 23 ]; then
+git clone https://github.com/hatakecnk/waspam
+fi
+if [ $u -eq 24 ]; then
+git clone https://github.com/masokky/instagram-tools.git
+fi
+if [ $u -eq 25 ]; then
+git clone https://github.com/Cabdulahi/pish
+fi
+if [ $u -eq 26 ]; then
+git clone https://github.com/thelinuxchoice/shellphish.git
+fi
+if [ $u -eq 27 ]; then
+git clone https://github.com/amenakun/Dork-Jahat
+fi
+if [ $u -eq 28 ]; then
+git clone https://github.com/yamanefkar/Hack-Tools
+fi
+if [ $u -eq 29 ]; then
+git clone https://github.com/dana-at-cp/backdoor-apk.git
+fi
+if [ $u -eq 30 ]; then
+git clone https://github.com/IlayTamvan/Report
+fi
+if [ $u -eq 31 ]; then
+git clone https://github.com/hasanbulat/tshark.git
+fi
+if [ $u -eq 32 ]; then
+git clone https://github.com/thelinuxchoice/spykey
+fi
+if [ $u -eq 33 ]; then
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+fi
+if [ $u -eq 34 ]; then
+git clone https://github.com/medbenali/CyberScan.git
+fi
+if [ $u -eq 35 ]; then
+git clone https://github.com/hatRiot/zarp
+fi
+if [ $u -eq 36 ]; then
+git clone https://gitlab.com/omest/interfarma.git
+fi
+if [ $u -eq 37 ]; then
+git clone https://github.com/OMEST/refploitv2.git
+fi
+if [ $u -eq 38 ]; then
+git clone https://gitlab.com/omest/sosmed.git
+fi
+if [ $u -eq 39 ]; then
+git clone https://github.com/MFDGaming/ubuntu-in-termux.git
+fi
+if [ $u -eq 40 ]; then
+git clone https://github.com/thelinuxchoice/thechoice
+fi
+if [ $u -eq 41 ]; then
+git clone https://github.com/sabri-zaki/EasY_HaCk
+fi
+if [ $u -eq 42 ]; then
+git clone https://github.com/maldevel/IPGeoLocation
+fi
+if [ $u -eq 43 ]; then
+git clone https://github.com/esc0rtd3w/wifi-hacker
+fi
+if [ $u -eq 44 ]; then
+git clone https://github.com/x-xsystm/maps.git
+fi
+if [ $u -eq 45 ]; then
+git clone https://github.com/p4kl0nc4t/Spammer-Grab
+fi
+if [ $u -eq 46 ]; then
+git clone https://github.com/Gameye98/Lazymux
+fi
+if [ $u -eq 47 ]; then
+git clone https://github.com/maurosoria/dirsearch
+fi
+if [ $u -eq 48 ]; then
+git clone https://github.com/king-hacking/MRKING.git
+fi
+if [ $u -eq 49 ]; then
+git clone https://github.com/ekultek/dagon.git
+fi
+if [ $u -eq 50 ]; then
+git clone https://github.com/Noxturnix/guardn
+fi
+if [ $u -eq 51 ]; then
+git clone https://github.com/princeyasinzone/virus_bot.git
+fi
+if [ $u -eq 52 ]; then
+git clone https://github.com/MoMuilWH/TmX-FW
+fi
+if [ $u -eq 53 ]; then
+git clone https://github.com/Manisso/fsociety
+fi
+if [ $u -eq 54 ]; then
+git clone https://github.com/thelinuxchoice/whatshack
+fi
+if [ $u -eq 55 ]; then
+git clone https://github.com/nasirxo/AK47
+fi
+if [ $u -eq 56 ]; then
+git clone https://github.com/Lulz3xploit/LittleBrother
+fi
+if [ $u -eq 57 ]; then
+git clone https://github.com/marioyhzkiell/bugreport.git
+fi
+if [ $u -eq 58 ]; then
+git clone https://github.com/s0md3v/Breacher.git
+fi
+if [ $u -eq 59 ]; then
+git clone https://github.com/anthrax3/DarkSploit
+fi
+if [ $u -eq 60 ]; then
+git clone https://github.com/0uss4ma/TOOL-DZ
+fi
+if [ $u -eq 61 ]; then
+git clone https://github.com/qw46478/BlackHacker
+fi
+if [ $u -eq 62 ]; then
+git clone https://github.com/yehia-hacker/Spy-Share
+fi
+if [ $u -eq 63 ]; then
+https://github.com/king-hacking/King-Hacking.git
+fi
+if [ $u -eq 64 ]; then
+git clone https://github.com/king-hacking/SPY-KING
+fi
+if [ $u -eq 65 ]; then
+git clone https://github.com/Amerlaceset/Amer
+fi
+if [ $u -eq 66 ]; then
+git clone https://github.com/Hider5/Malicious
+fi
+if [ $u -eq 67 ]; then
+https://github.com/Junior60/vbug.git
+fi
+if [ $u -eq 68 ]; then
+git clone https://github.com/Ahmed-Altorky-Android/AHT.Hacker.git
+fi
+if [ $u -eq 69 ]; then
+git clone https://github.com/yehia-hacker/new
+fi
+if [ $u -eq 70 ]; then
+git clone https://github.com/jmaxmoh/Jmaxojan
+fi
+if [ $use = 71 ]
+then
+clear
+exit
+"Good By"
+echo " Script By < Mahmoud Mohamed " 
+else
+echo "Please Enter Valid Data"
+fi
